2776665efb383548817a8d12caf1835fd704013f
dramaeasy <screamo14@aim.com>
2015-01-31 14:25:18 -0800

    Modifying AI to blink colors and adding a 'runaway from player' state

---
+++ Assets/EnemyColor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyColor : MonoBehaviour {
+
+	SkinnedMeshRenderer mesh;
+	float blinkTime = 1f;
+	float timer = 0f;
+	Color redd;
+	// Use this for initialization
+	void Start () 
+	{
+		mesh = GetComponent<SkinnedMeshRenderer> ();
+		redd = new Color (1, 0, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= blinkTime)
+		{
+			if(mesh.renderer.material.color == Color.blue)
+			{
+				Debug.Log("im  red");
+				mesh.material.color = Color.white;
+			}
+			else
+			{
+				Debug.Log("Im blue");
+				mesh.material.color = Color.blue;
+			}
+
+			timer = 0f;
+		}
+	}
+}
