ac4aa5780b301a4e61135beba6daa23c7a71063b
mrpallyguy <sla002@ucr.edu>
2015-02-12 19:53:15 -0800

    Fixed Win-Screen

--- Assets/RestartGameListener.cs
+++ Assets/RestartGameListener.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class RestartGameListener : MonoBehaviour {
 
+ 	void OnGui(){
+		GUI.Label(new Rect(0,0,Screen.width,Screen.height),"Press Enter to Restart Game");
+	}
 	void Update () 
 	{
 		if (Input.GetKeyDown (KeyCode.Return)) {
